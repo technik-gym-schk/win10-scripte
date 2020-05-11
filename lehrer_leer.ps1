1ffe3cc2155ff509102e7f9d01204335b8dc7953
technik-gym-schk <64584239+technik-gym-schk@users.noreply.github.com>
2020-05-11 18:15:37 +0200

    Add files via upload

---
+++ lehrer_leer.ps1
@@ -0,0 +1,7 @@
+﻿$users = Get-LocalUser -Name l*
+foreach($user in $users){
+    $folders = Get-ChildItem -Path "C:\Users\$($user.name)"
+    foreach($folder in $folders){
+        Remove-Item "C:\Users\$($user.name)\$($folder.name)\*" -Force -Recurse
+    }
+}
